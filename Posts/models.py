73b214294274cdfd17b18c38e96a12e54a0695a1
ntrutuo1 <tuongnt1907xx@gmail.com>
2024-07-29 19:34:31 +0700

    Modify arc.settings, log.views, log.urls, add templates for login, register, home

---
+++ Posts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Post(models.Model):
+    content = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    video = models.FileField(upload_to='videos/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content[:50] if self.content else "Media Post"
